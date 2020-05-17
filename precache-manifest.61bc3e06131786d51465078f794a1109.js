fa6a0eb67a9ac57303d87273dc0ba62372d3c2af
danjonesdev <dannyjones360@gmail.com>
2020-05-17 13:57:24 +0100

    Updates

---
+++ precache-manifest.61bc3e06131786d51465078f794a1109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e7992ca4396bea55b88d0ed020ad53",
+    "url": "/next-pattern-library/index.html"
+  },
+  {
+    "revision": "c35b890b4f519f84a5f3",
+    "url": "/next-pattern-library/static/css/main.227d9887.chunk.css"
+  },
+  {
+    "revision": "072847d5cdfb8b6d390f",
+    "url": "/next-pattern-library/static/js/2.af18bf48.chunk.js"
+  },
+  {
+    "revision": "c35b890b4f519f84a5f3",
+    "url": "/next-pattern-library/static/js/main.c7d92c1a.chunk.js"
+  },
+  {
+    "revision": "75fc07b7268c30dc76d9",
+    "url": "/next-pattern-library/static/js/runtime~main.fc2eebf2.js"
+  }
+]);
