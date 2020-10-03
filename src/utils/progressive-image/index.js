44b11a246deffd0cd4fead6610d620ede5e5c1d5
danjonesdev <dannyjones360@gmail.com>
2020-10-03 06:24:19 +0100

    Merge conflicts

--- src/utils/progressive-image/index.js
+++ src/utils/progressive-image/index.js
@@ -4,22 +4,6 @@ import React, { useEffect, useState, useRef } from 'react';
  * ProgressiveImage.
  */
 
-<<<<<<< HEAD
-const ProgressiveImage = React.memo(props => {
-  const { placeholder, dimensions, alt, src } = props;
-
-  // Loaded states
-  const [highResLoaded, setHighResLoaded] = useState(false);
-  const [lowResLoaded, setLowResLoaded] = useState(false);
-
-  // Set loaded states
-  const handleHighResLoaded = () => {
-    if (!highResLoaded) setHighResLoaded(true);
-  };
-  const handleLowResLoaded = () => {
-    if (!lowResLoaded) setLowResLoaded(true);
-  };
-=======
 const useImageLoaded = () => {
   const [loaded, setLoaded] = useState(false);
   const ref = useRef();
@@ -38,37 +22,16 @@ const useImageLoaded = () => {
 const ProgressiveImage = props => {
   const { placeholder, dimensions, alt, src } = props;
   const [ref, loaded, onLoad] = useImageLoaded();
->>>>>>> 2af64d8d4deadc43d6983dff0845bda5aae71b23
 
   return (
     <React.Fragment>
       <img
-<<<<<<< HEAD
-=======
         ref={ref}
->>>>>>> 2af64d8d4deadc43d6983dff0845bda5aae71b23
         className="image__loading"
         alt="loading..."
         src={placeholder}
         style={{
           ...dimensions,
-<<<<<<< HEAD
-          opacity: !highResLoaded && !lowResLoaded ? '1' : '0'
-        }}
-        onLoad={handleLowResLoaded}
-      />
-
-      <img
-        className="w-100  image"
-        style={dimensions}
-        alt={alt}
-        src={src}
-        onLoad={handleHighResLoaded}
-      />
-    </React.Fragment>
-  );
-});
-=======
           opacity: !loaded ? '1' : '0'
         }}
         onLoad={onLoad}
@@ -78,6 +41,5 @@ const ProgressiveImage = props => {
     </React.Fragment>
   );
 };
->>>>>>> 2af64d8d4deadc43d6983dff0845bda5aae71b23
 
 export default ProgressiveImage;
