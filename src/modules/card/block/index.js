e59edaee6903b8831badaad3d2efcf976555f10a
danjonesdev <dannyjones360@gmail.com>
2020-10-13 20:34:05 +0100

    Try a different way to render card labels

--- src/modules/card/block/index.js
+++ src/modules/card/block/index.js
@@ -20,32 +20,16 @@ export default function CardBlock(props) {
 
   const hasOnClick = onClick && { onClick };
 
-  const renderContents = () => {
-    const renderLabels = () => {
-      if (!labelBlock.length) return false;
-
-      const listItems = labelBlock.map(label => (
-        <span key={label} className="card__label">
-          {label}
-        </span>
-      ));
-
-      return <div className="card__label-block">{listItems}</div>;
-    };
-
-    return (
-      <div className="card__contents-wrapper">
-        {labelBlock && renderLabels()}
-        {title && <div className="card__title">{title}</div>}
-        {description && <div className="card__description">{description}</div>}
-      </div>
-    );
-  };
-
   return (
     <div className="card  card--small">
       {image && <div className="card__image">{image}</div>}
-      {renderContents()}
+      <div className="card__contents-wrapper">
+        {labelBlock.length && (
+          <div className="card__labels">{[...labelBlock]}</div>
+        )}
+        {title && <div className="card__title">{title}</div>}
+        {description && <div className="card__description">{description}</div>}
+      </div>
       <div className="card__button-wrapper">{button}</div>
     </div>
   );
