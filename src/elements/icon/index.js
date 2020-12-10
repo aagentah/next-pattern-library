7a2cbc6a2e8007761051c3491b2896e5b98cd1d8
danjonesdev <dannyjones360@gmail.com>
2020-12-10 16:33:40 +0000

    Remove far from font-awesome

--- src/elements/icon/index.js
+++ src/elements/icon/index.js
@@ -3,13 +3,10 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/fontawesome-free-regular';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Icon(props) {
-  library.add(fas);
-  library.add(fab);
-  library.add(far);
+  library.add(fas, fab);
 
   return (
     <div className="icon-wrapper">
