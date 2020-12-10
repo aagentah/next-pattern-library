1e511c44c82df4442c0002ce6dcdcc61cce51d39
danjonesdev <dannyjones360@gmail.com>
2020-12-10 15:14:01 +0000

    Add all font-awesome icons

--- src/elements/icon/index.js
+++ src/elements/icon/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/fontawesome-free-regular';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Icon(props) {
   library.add(fas);
+  library.add(fab);
+  library.add(far);
 
   return (
     <div className="icon-wrapper">
